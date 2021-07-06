6f881afab996848e4be21e75efa6f2f35ed86f2c
lukas <lukas.luetolf@gmail.com>
2021-07-06 17:08:52 +0200

    Add database name to start up logging.

--- microservices/payable-service/src/main/java/ch/canaweb/microservices/core/payable/PayableServiceApplication.java
+++ microservices/payable-service/src/main/java/ch/canaweb/microservices/core/payable/PayableServiceApplication.java
@@ -10,8 +10,10 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mapping.context.MappingContext;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import org.springframework.data.mongodb.core.index.Index;
 import org.springframework.data.mongodb.core.index.IndexResolver;
 import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver;
 import org.springframework.data.mongodb.core.index.ReactiveIndexOperations;
@@ -29,7 +31,8 @@ public class PayableServiceApplication {
 
         String mongodDbHost = ctx.getEnvironment().getProperty("spring.data.mongodb.host");
         String mongodDbPort = ctx.getEnvironment().getProperty("spring.data.mongodb.port");
-        LOG.info("Connected to MongoDb: " + mongodDbHost + ":" + mongodDbPort);
+        String mongodDbDatabase = ctx.getEnvironment().getProperty("spring.data.mongodb.database");
+        LOG.info("Connected to MongoDb: " + mongodDbHost + ":" + mongodDbPort + "/" + mongodDbDatabase);
 
         String canaweb_env = ctx.getEnvironment().getProperty("canaweb_env");
         LOG.info("CanaWeb Env: " + canaweb_env);
@@ -47,6 +50,6 @@ public class PayableServiceApplication {
         ReactiveIndexOperations indexOps = mongoTemplate.indexOps(PayableEntity.class);
         resolver.resolveIndexFor(PayableEntity.class).forEach(e -> indexOps.ensureIndex(e).block());
 
-//        mongoTemplate.indexOps("payables").ensureIndex(new Index("payableId", Sort.Direction.ASC).unique());
+        mongoTemplate.indexOps("payables").ensureIndex(new Index("payableId", Sort.Direction.ASC).unique());
     }
 }
