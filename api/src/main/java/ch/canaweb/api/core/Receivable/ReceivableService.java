f5a19a4875f4098d7bc258930f65b0ac0b51c975
lukas <lukas.luetolf@gmail.com>
2021-07-06 20:36:40 +0200

    Implement ReceivableService and first Receivable test.

--- api/src/main/java/ch/canaweb/api/core/Receivable/ReceivableService.java
+++ api/src/main/java/ch/canaweb/api/core/Receivable/ReceivableService.java
@@ -1,5 +1,7 @@
 package ch.canaweb.api.core.Receivable;
 
+import ch.canaweb.api.core.Receivable.Receivable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -9,11 +11,37 @@ public interface ReceivableService {
     @GetMapping(
             value    = "/receivable/{receivableId}",
             produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
     Mono<Receivable> getReceivable(@PathVariable int receivableId);
+
+    @GetMapping(
+            value    = "/payable/field/{fieldId}",
+            produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    Flux<Receivable> getReceivableForField(@PathVariable int fieldId);
 
     @GetMapping(
             value    = "/receivable",
             produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
     Flux<Receivable> getAllReceivables();
 
+    @PostMapping(
+            path = "/payable",
+            consumes = "application/json",
+            produces = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    Mono<Receivable> createReceivable(@RequestBody Receivable body);
+
+    @PutMapping(
+            path = "/payable",
+            consumes = "application/json",
+            produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    Mono<Receivable> updateReceivable(@RequestBody Receivable body);
+
+    @DeleteMapping(
+            path = "/payable/{payableId}")
+    @ResponseStatus(HttpStatus.OK)
+    Mono<Void> deleteReceivable(@PathVariable int payableId);
 }
