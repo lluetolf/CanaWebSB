14876be9a8be3f5f32e3fbe39272b02d7ee53138
lukas <lukas.luetolf@gmail.com>
2021-07-06 17:38:20 +0200

    Create Payable microservice.

---
+++ microservices/receivable-service/src/test/java/ch/canaweb/microservices/core/receivable/ReceivableServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.canaweb.microservices.core.receivable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceivableServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
