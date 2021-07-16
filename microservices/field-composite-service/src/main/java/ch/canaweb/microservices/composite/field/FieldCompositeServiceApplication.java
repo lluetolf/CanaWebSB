38bd8878eba75d65bbc83794d01ef9c9ad7e3488
lukas <lukas.luetolf@gmail.com>
2021-07-16 16:31:04 +0200

    Add SpringFox.

--- microservices/field-composite-service/src/main/java/ch/canaweb/microservices/composite/field/FieldCompositeServiceApplication.java
+++ microservices/field-composite-service/src/main/java/ch/canaweb/microservices/composite/field/FieldCompositeServiceApplication.java
@@ -2,15 +2,33 @@ package ch.canaweb.microservices.composite.field;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import static java.util.Collections.emptyList;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @ComponentScan("ch.canaweb")
 public class FieldCompositeServiceApplication {
+    @Value("${api.common.version}")           String apiVersion;
+    @Value("${api.common.title}")             String apiTitle;
+    @Value("${api.common.description}")       String apiDescription;
+
     private static final Logger LOG = LoggerFactory.getLogger(FieldCompositeServiceApplication.class);
 
     public static void main(String[] args) {
@@ -18,6 +36,26 @@ public class FieldCompositeServiceApplication {
         ConfigurableApplicationContext ctx = SpringApplication.run(FieldCompositeServiceApplication.class, args);
 
         LOG.info("Tripple Hello");
+    }
+
+    @Bean
+    public Docket apiDocumentation() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(basePackage("ch.canaweb.microservices.composite.field.services"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(new ApiInfo(
+                        apiTitle,
+                        apiDescription,
+                        apiVersion,
+                        null,
+                        null,
+                        null,
+                        null,
+                        Collections.emptyList()
+                ));
     }
 
 }
